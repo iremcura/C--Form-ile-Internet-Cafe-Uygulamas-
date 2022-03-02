dca1190a8aa7cd2f146ca81716daf80983b9ea21
iremcura <ireemcc1@gmail.com>
2022-03-02 20:23:58 +0300

    internet cafe ücret hesaplama ekranı
    c# form alıştırmalar

---
+++ INTERNET CAFE/INTERNET CAFE/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿
+namespace INTERNET_CAFE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.masa1Button = new System.Windows.Forms.Button();
+            this.masa2Button = new System.Windows.Forms.Button();
+            this.masa3Button = new System.Windows.Forms.Button();
+            this.masa4Button = new System.Windows.Forms.Button();
+            this.masa5Button = new System.Windows.Forms.Button();
+            this.masa6Button = new System.Windows.Forms.Button();
+            this.txtMasa = new System.Windows.Forms.TextBox();
+            this.txtSure = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // masa1Button
+            // 
+            this.masa1Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.masa1Button.Location = new System.Drawing.Point(368, 47);
+            this.masa1Button.Name = "masa1Button";
+            this.masa1Button.Size = new System.Drawing.Size(64, 53);
+            this.masa1Button.TabIndex = 0;
+            this.masa1Button.Text = "1";
+            this.masa1Button.UseVisualStyleBackColor = true;
+            this.masa1Button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // masa2Button
+            // 
+            this.masa2Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.masa2Button.Location = new System.Drawing.Point(368, 133);
+            this.masa2Button.Name = "masa2Button";
+            this.masa2Button.Size = new System.Drawing.Size(64, 54);
+            this.masa2Button.TabIndex = 1;
+            this.masa2Button.Text = "2";
+            this.masa2Button.UseVisualStyleBackColor = true;
+            // 
+            // masa3Button
+            // 
+            this.masa3Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.masa3Button.Location = new System.Drawing.Point(452, 47);
+            this.masa3Button.Name = "masa3Button";
+            this.masa3Button.Size = new System.Drawing.Size(66, 53);
+            this.masa3Button.TabIndex = 2;
+            this.masa3Button.Text = "3";
+            this.masa3Button.UseVisualStyleBackColor = true;
+            // 
+            // masa4Button
+            // 
+            this.masa4Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.masa4Button.Location = new System.Drawing.Point(452, 133);
+            this.masa4Button.Name = "masa4Button";
+            this.masa4Button.Size = new System.Drawing.Size(66, 54);
+            this.masa4Button.TabIndex = 3;
+            this.masa4Button.Text = "4";
+            this.masa4Button.UseVisualStyleBackColor = true;
+            // 
+            // masa5Button
+            // 
+            this.masa5Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.masa5Button.Location = new System.Drawing.Point(543, 47);
+            this.masa5Button.Name = "masa5Button";
+            this.masa5Button.Size = new System.Drawing.Size(66, 53);
+            this.masa5Button.TabIndex = 4;
+            this.masa5Button.Text = "5";
+            this.masa5Button.UseVisualStyleBackColor = true;
+            // 
+            // masa6Button
+            // 
+            this.masa6Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.masa6Button.Location = new System.Drawing.Point(543, 133);
+            this.masa6Button.Name = "masa6Button";
+            this.masa6Button.Size = new System.Drawing.Size(66, 54);
+            this.masa6Button.TabIndex = 5;
+            this.masa6Button.Text = "6";
+            this.masa6Button.UseVisualStyleBackColor = true;
+            // 
+            // txtMasa
+            // 
+            this.txtMasa.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtMasa.Location = new System.Drawing.Point(444, 238);
+            this.txtMasa.Multiline = true;
+            this.txtMasa.Name = "txtMasa";
+            this.txtMasa.Size = new System.Drawing.Size(165, 33);
+            this.txtMasa.TabIndex = 6;
+            // 
+            // txtSure
+            // 
+            this.txtSure.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSure.Location = new System.Drawing.Point(444, 286);
+            this.txtSure.Multiline = true;
+            this.txtSure.Name = "txtSure";
+            this.txtSure.Size = new System.Drawing.Size(165, 36);
+            this.txtSure.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(331, 238);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Masa No :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(302, 289);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Toplam Süre :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(381, 410);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(128, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "TOPLAM UCRET";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(540, 410);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "label4";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(455, 344);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 48);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "HESAPLA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::INTERNET_CAFE.Properties.Resources.My_Computer_icon;
+            this.pictureBox1.Location = new System.Drawing.Point(63, 61);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(163, 174);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Mongolian Baiti", 13F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label5.Location = new System.Drawing.Point(3, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(329, 24);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "ÇUKUROVA İNTERNET CAFE";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGreen;
+            this.ClientSize = new System.Drawing.Size(635, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSure);
+            this.Controls.Add(this.txtMasa);
+            this.Controls.Add(this.masa6Button);
+            this.Controls.Add(this.masa5Button);
+            this.Controls.Add(this.masa4Button);
+            this.Controls.Add(this.masa3Button);
+            this.Controls.Add(this.masa2Button);
+            this.Controls.Add(this.masa1Button);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "ÇUKUROVA İNTERNET CAFE";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button masa1Button;
+        private System.Windows.Forms.Button masa2Button;
+        private System.Windows.Forms.Button masa3Button;
+        private System.Windows.Forms.Button masa4Button;
+        private System.Windows.Forms.Button masa5Button;
+        private System.Windows.Forms.Button masa6Button;
+        private System.Windows.Forms.TextBox txtMasa;
+        private System.Windows.Forms.TextBox txtSure;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ INTERNET CAFE/INTERNET CAFE/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace INTERNET_CAFE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+     
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+       
+            int masaNo, toplamSure, ucret;
+
+            masaNo = Convert.ToInt32(txtMasa.Text);
+            toplamSure = Convert.ToInt32(txtSure.Text);
+            ucret = 5 * toplamSure;
+
+            if(masaNo == 1)
+            {
+
+                MessageBox.Show($"{masaNo} NUMARALI MASA ÖDENECEK TUTAR {ucret} "); 
+            }
+            else if(masaNo == 2)
+            {
+
+                MessageBox.Show($"{masaNo} NUMARALI MASA ÖDENECEK TUTAR {ucret} ");
+            }
+            else if (masaNo == 3)
+            {
+
+                MessageBox.Show($"{masaNo} NUMARALI MASA ÖDENECEK TUTAR {ucret} ");
+            }
+            else if (masaNo == 4)
+            {
+
+                MessageBox.Show($"{masaNo} NUMARALI MASA ÖDENECEK TUTAR {ucret} ");
+            }
+            else if (masaNo == 5)
+            {
+
+                MessageBox.Show($"{masaNo} NUMARALI MASA ÖDENECEK TUTAR {ucret} ");
+            }
+            else if (masaNo == 6)
+            {
+
+                MessageBox.Show($"{masaNo} NUMARALI MASA ÖDENECEK TUTAR {ucret} ");
+            }
+            else
+            {
+                
+                MessageBox.Show($"GEÇERLİ BİR MASA NUMARASI GİRİNİZ ");
+                
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
